786cc1a672404eab730a70618cd51adfb86ed6bf
XozzinAll-hub <allancastro02@hotmail.com>
2025-09-25 04:07:40 +0000

    comit prueba de ramas

--- Ejercicio_1.C
+++ Ejercicio_1.C
@@ -4,19 +4,14 @@ int factorial(int n) {
     int i = 1;
     while (n > 1) {
         i = i * n;
-        n = n - 1; 
+        n = n - 1;  
     }
     return i;
-}
+}  
 
 int main(int argc, char *argv[]) {
-    int numero;
-    
-    printf("Ingrese un número para calcular su factorial: ");
-    scanf("%d", &numero);
-    
-    int resultado = factorial(numero);
-    printf("%d! = %d\n", numero, resultado);
-    
+    int fac4 = factorial(4);
+    int fac5 = factorial(5);
+    printf("4! = %d, 5! = %d\n", fac4, fac5);
     return 0;
 }
