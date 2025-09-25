733b35899213e285c7b3280aff39e3031c077830
XozzinAll-hub <allancastro02@hotmail.com>
2025-09-25 04:38:57 +0000

    modificacion del ejecicio para insetar el valor

--- Ejercicio_1.C
+++ Ejercicio_1.C
@@ -4,14 +4,19 @@ int factorial(int n) {
     int i = 1;
     while (n > 1) {
         i = i * n;
-        n = n - 1;  
+        n = n - 1;
     }
     return i;
-}  
+}
 
-int main(int argc, char *argv[]) {
-    int fac4 = factorial(4);
-    int fac5 = factorial(5);
-    printf("4! = %d, 5! = %d\n", fac4, fac5);
+int main(int argc, char *argv[]) {//modificacion para el ingreso de datos
+    int numero;
+    
+    printf("Escriba su numero a calcular el factorial: ");
+    scanf("%d", &numero);
+    
+    int resultado = factorial(numero);
+    printf("%d! = %d\n", numero, resultado);
+    
     return 0;
 }
